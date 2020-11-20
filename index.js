26176b48887ce1df5444f7ee4e6e45116cc78771
PrestonKramer <kramerp@oregonstate.edu>
2020-11-20 10:59:53 -0700

    1

---
+++ index.js
@@ -0,0 +1,52 @@
+let express = require('express');
+
+//create connection as shown in lecture video
+let mysql = require('./dbcon.js');
+
+let app = express();
+let handlebars = require('express-handlebars').create({defaultLayout:'main'});
+let bodyParser = require('body-parser');
+let session = require('express-session');
+
+app.use(session({secret:'1a2b3c'}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+
+app.use(express.static('public'));
+
+//renders home page
+app.get('/reset-table',function(req,res,next){
+  let context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+    let createString = "CREATE TABLE todo(" +
+    "id INT PRIMARY KEY AUTO_INCREMENT," +
+    "name VARCHAR(255) NOT NULL," +
+    "done BOOLEAN," +
+    "due DATE)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
