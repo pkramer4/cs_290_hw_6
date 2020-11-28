10de921034db0d3c15acadf6ecbee353ced0cd76
PrestonKramer <kramerp@oregonstate.edu>
2020-11-28 16:04:40 -0700

    3

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  host  : 'localhost',
-  user  : 'student',
-  password: 'default',
-  database: 'student'
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs290_kramerp',
+  password        : '5685',
+  database        : 'cs290_kramerp'
 });
 
 module.exports.pool = pool;
--- index.js
+++ index.js
@@ -20,30 +20,42 @@ app.set('port', process.argv[2]);
 app.use(express.static('public'));
 
 //renders home page
-app.get('/',function(req,res,next){
-  let context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-    let createString = "CREATE TABLE todo(" +
-    "id INT PRIMARY KEY AUTO_INCREMENT," +
-    "name VARCHAR(255) NOT NULL," +
-    "done BOOLEAN," +
-    "due DATE)";
-    mysql.pool.query(createString, function(err){
-      context.results = "Table reset";
-      res.render('home',context);
-    })
-  });
-});
+// app.get('/',function(req,res,next){
+//   let context = {};
+//   mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+//     let createString = "CREATE TABLE todo(" +
+//     "id INT PRIMARY KEY AUTO_INCREMENT," +
+//     "name VARCHAR(255) NOT NULL," +
+//     "done BOOLEAN," +
+//     "due DATE)";
+//     mysql.pool.query(createString, function(err){
+//       context.results = "Table reset";
+//       res.render('home',context);
+//     })
+//   });
+// });
+//
+// app.get('/insert',function(req,res,next){
+//   var context = {};
+//   mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
+//     if(err){
+//       next(err);
+//       return;
+//     }
+//     context.results = "Inserted id " + result.insertId;
+//     res.render('home',context);
+//   });
+// });
 
-app.get('/insert',function(req,res,next){
+app.get('/',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
+  mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
     if(err){
       next(err);
       return;
     }
-    context.results = "Inserted id " + result.insertId;
-    res.render('home',context);
+    context.results = JSON.stringify(rows);
+    res.render('home', context);
   });
 });
 
